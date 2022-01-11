aaed9b8b247f98c3b9e4516f75a13d19317228ca
Henryla51 <henryhen2019@gmail.com>
2022-01-11 10:11:55 -0500

    fw

--- api-project/indexJS/main.js
+++ api-project/indexJS/main.js
@@ -1,90 +1,22 @@
 import { arrayData } from "./array.js";
 import { DOMSelectors } from "./dom";
 
-function defaultCardGenerate(el) {
-  el.data.forEach((character) => {
-    if (character.data.name === null) {
-      if (character.data.icon != null) {
-        if (character.data.description != null) {
-          DOMSelectors.cards.insertAdjacentHTML(
-            "beforeend",
-            `<div class="card" id="${film.id}">
-    <h2 class="movieName">${film.data.name}</h2>
-    <img
-      src="${film.data.imageURL}"
-    />
-    <div class="text">`
-          );
-        }
-      }
-    }
-  });
-  DOMSelectors.inputField.value = "";
+const apiEntry = "https://api.disneyapi.dev/characters";
+async function fetchData(apiEntry) {
+  try {
+    const response = await fetch(apiEntry);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 }
+fetchData(apiEntry);
 
-arrayData.then((val) => {
-  defaultCardGenerate(val);
-});
-function cardGenerate(el) {
-  el.data.forEach((character) => {
-    if (character.data.name == null) return;
-    const check = character.data.name.toLowerCase();
-    if (check == DOMSelectors.inputField.value.toLowerCase()) {
-      if (character.data.imageURL === null) {
-        DOMSelectors.cards.insertAdjacentHTML(
-          "beforeend",
-          `<div class="card" id="${film.id}">
-      <h2 class="movieName">${film.data.name}</h2>
-      <img
-        
-      />
-      <div class="text">`
-        );
-      } else {
-        DOMSelectors.cards.insertAdjacentHTML(
-          "beforeend",
-          `<div class="card" id="${film.id}">
-      <h2 class="movieName">${film.disney.name}</h2>
-      <img
-        
-      />`
-        );
-      }
-    } else {
-      if (character.attributes.description === null) {
-        if (character.data.imageURL === null) {
-          DOMSelectors.cards.insertAdjacentHTML(
-            "beforeend",
-            `<div class="card" id="${film.id}">
-      <h2 class="movieName">${film.data.name}</h2>
-      <img
-        
-      />`
-          );
-        } else {
-          DOMSelectors.cards.insertAdjacentHTML(
-            "beforeend",
-            `<div class="card" id="${film.id}">
-      <h2 class="movieName">${film.data.name}</h2>
-      <img
-        src="${film.data.imageURL}"
-      />
-      <div class="text">`
-          );
-        }
-      } else {
-        DOMSelectors.cards.insertAdjacentHTML(
-          "beforeend",
-          `<div class="card" id="${film.id}">
-    <h2 class="movieName">${film.data.name}</h2>
-    <img
-      src="${film.data.imageURL}"
-    />
-    <div class="text">`
-        );
-      }
-    }
-  });
-  console.log("test");
-  DOMSelectors.inputField.value = "";
-}
+const apiResponseDOM = document.getElementById("api-response");
+const putQuoteInHTML = async () => {
+  const quote = await fetchData(apiEntry);
+  apiResponseDOM.innerHTML = `Quote: ${quote.content}`;
+};
+putQuoteInHTML();
