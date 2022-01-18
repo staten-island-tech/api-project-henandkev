7975267429e956309834ac118e8f306bb95a6c0a
Kevin <quickmathisgood@gmail.com>
2022-01-18 10:09:26 -0500

    make search button

---
+++ api-project/JS/array.js
@@ -0,0 +1,12 @@
+const url = "https://rickandmortyapi.com/api/character";
+
+async function getData(URL) {
+    try {
+        const response = await fetch(URL);
+        const Data = await response.json();
+        console.log(Data);
+        return Data;
+    } catch (error) {
+        console.log(error);
+    }
+}
