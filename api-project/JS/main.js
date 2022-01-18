30efa03ebe54d3b213ba01858aa7140b691582cc
Henryla51 <henryhen2019@gmail.com>
2022-01-18 09:42:08 -0500

    .

---
+++ api-project/JS/main.js
@@ -0,0 +1,37 @@
+import { Data } from "./array.js";
+
+const DOMSelectors = {
+  cards: document.querySelector(".cards"),
+};
+
+function defaultCardGenerate(el) {
+  el.results.forEach((characters) => {
+    DOMSelectors.cards.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card" id="${characters.id}">
+    <h2 class="characterName">${characters.name}</h2>
+    <img src="${characters.image}"/>
+    <div class="text">
+    <h3>
+    ${characters.status}</h3>
+      <h3>
+        ${characters.origin.name}
+      </h3>`
+    );
+  });
+  DOMSelectors.inputField.value = "";
+}
+
+Data.then((what) => {
+  defaultCardGenerate(what);
+});
+
+// Open the full screen search box
+function openSearch() {
+  document.getElementById("cards").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+  document.getElementById("cards").style.display = "none";
+}
